1b5fa30f679292949ceead41761393b222adfd12
Đặng Ngọc Tiến <20127641@student.hcmus.edu.vn>
2022-12-16 17:05:28 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+*   
+*   Create by Đặng Ngọc Tiến
+*   Date ${DATE} - ${TIME}
+*   Description: ...
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

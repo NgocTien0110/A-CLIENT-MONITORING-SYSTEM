25e2495dda233cf27f18d98a4f8ac7fb4fc5b25f
Đặng Ngọc Tiến <20127641@student.hcmus.edu.vn>
2022-12-19 21:09:38 +0700

    file Test để test connect 2 client

---
+++ src/Client/MainTest.java
@@ -0,0 +1,14 @@
+package Client;
+
+/**
+ * Client
+ * Create by Đặng Ngọc Tiến
+ * Date 12/19/2022 - 5:49 PM
+ * Description: ...
+ */
+public class MainTest {
+    public static void main(String arg[])
+    {
+        new InitClient();
+    }
+}
